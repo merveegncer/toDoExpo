f61c57a88312ac8aff9ce771a3581227dc3dae30
merveegncer <merveegncer@gmail.com>
2023-05-23 23:32:13 +0300

    e mail dogrulamasi eklendi modal eklendi

--- screens/ListToDo.js
+++ screens/ListToDo.js
@@ -1,9 +1,15 @@
-import {View, Button} from 'react-native';
+import {View, Button, Text, SafeAreaView, Modal} from 'react-native';
 import AppStyles from '../styles/AppStyles';
 import { auth } from '../firebase';
 import { signOut } from 'firebase/auth';
+import InlineTextButton from '../components/InlineTextButton';
+import React from 'react';
+import AddToDoModal from '../components/AddToDoModal';
 
 export default function ListToDo({navigation, route}){
+
+    let [modalVisible, setModalVisible]=React.useState(false);
+
     let logout=()=>{
         signOut(auth).then(() => {
             
@@ -12,9 +18,51 @@ export default function ListToDo({navigation, route}){
 
     }
 
-    return(
-        <View style = {AppStyles.container}>
-            <Button title='Log Out' onPress={logout}/>
+    let showContent=()=>{
+        return(
+            <View style={AppStyles.containerPadding}>
+                <Button 
+                title="Add ToDo" 
+                onPress={()=>setModalVisible(true)} 
+                 color={"#FF9549"}/>
+            </View>
+        );
+        
+    };
+
+    let showSendVerificationEmail=()=>{
+        return(
+        <View>
+              <Text> Please verifiy your email</Text>
+              <Button title= "send Email Verification" onPress={()=> sendEmailVerification(auth.currentUser)}/>
         </View>
+        )
+    };
+
+    return(
+        <SafeAreaView style = {AppStyles.containerPadding}>
+            <View style={[AppStyles.rowContainer, AppStyles.rightAligned, AppStyles.rightMargin]}>
+                <InlineTextButton text="Manage Account" color= "#726DA8"/>
+            </View>
+
+            <Modal
+            animationType='slide'
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={()=>setModalVisible(false)}>
+            
+            
+            <AddToDoModal 
+                onClose={()=> setModalVisible(false)}
+                addToDo={(todo)=> console.log(todo)}
+            />
+
+            
+            </Modal>
+
+            <Text style={AppStyles.Headers}>ToDo</Text>
+            {auth.currentUser.emailVerified ? showContent(): showSendVerificationEmail()}
+            <Button title='Log Out' onPress={logout}/>
+        </SafeAreaView>
     )
 }
