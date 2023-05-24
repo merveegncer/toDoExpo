bd8ba471830b67d0ef3125c02bea6a5d12aa3ffc
merveegncer <merveegncer@gmail.com>
2023-05-24 04:01:48 +0300

    databse ekledim

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 import{initializeApp} from "firebase/app"
 import { getAuth} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 
 
 const firebaseConfig = {
@@ -13,9 +15,11 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const db = getFirestore();
 
 
   export{
-    auth
+    auth,
+    db
   }
 
